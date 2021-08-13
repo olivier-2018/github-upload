b94d20db1e9f8649ed44552a01261481194c0027
Jack <jack@example.com>
2021-08-13 14:00:47 +0200

    adding copy hello world

---
+++ hello_world2.py
@@ -0,0 +1,10 @@
+import numpy as np
+from random import random
+
+X = np.array([1,2,3,4])
+Y = np.random.rand(4)
+
+print ("hello world !")
+print ("X= ", str(X))
+print ("Y= ", str(Y))
+print ("X+Y= ", np.dot(X,Y))
