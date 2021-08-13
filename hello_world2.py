b846ba020ef5dc051056bcbbdac0fe574f484641
Jack <jack@example.com>
2021-08-13 14:27:08 +0200

    modified helloworld2, changed branch

--- hello_world2.py
+++ hello_world2.py
@@ -1,10 +1,12 @@
 import numpy as np
 from random import random
 
-X = np.array([1,2,3,4])
+#X = np.array([1,2,3,4])
+X = np.random.rand(4)
 Y = np.random.rand(4)
 
 print ("hello world !")
 print ("X= ", str(X))
 print ("Y= ", str(Y))
 print ("X+Y= ", np.dot(X,Y))
+
